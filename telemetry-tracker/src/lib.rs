858d526cc68e4c78f2a0c6abb8220dfaf2fd9f9c
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-22 19:57:23 +0100

    add default for url and chain

--- telemetry-tracker/src/lib.rs
+++ telemetry-tracker/src/lib.rs
@@ -1,11 +1,13 @@
 #[macro_use]
 extern crate anyhow;
 
-use futures::SinkExt;
+use futures::{StreamExt, SinkExt};
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
 
 mod events;
+
+const DEFAULT_TELEMETRY: &'static str = "wss://telemetry-backend.w3f.community/feed";
 
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
@@ -23,7 +25,16 @@ impl AsRef<str> for Chain {
     }
 }
 
-async fn run_listener(url: &str, chain: Chain) -> Result<()> {
+impl Default for Chain {
+    fn default() -> Self {
+        Chain::Polkadot
+    }
+}
+
+async fn run_listener(url: Option<&str>, chain: Option<Chain>) -> Result<()> {
+    let url = url.unwrap_or(DEFAULT_TELEMETRY);
+    let chain = chain.unwrap_or(Default::default());
+
     // Open stream.
     let (mut stream, _) = connect_async(url)
         .await
@@ -34,6 +45,15 @@ async fn run_listener(url: &str, chain: Chain) -> Result<()> {
         .send(Message::text(format!("subscribe:{}", chain.as_ref())))
         .await
         .map_err(|err| anyhow!("Failed to subscribe to chain {}: {:?}", chain.as_ref(), err))?;
+
+    while let Some(msg) = stream.next().await {
+        match msg? {
+            Message::Binary(content) => {
+                println!(">> {}", String::from_utf8_lossy(&content));
+            }
+            _ => {},
+        }
+    }
 
     Ok(())
 }
